f17fd490f6a00f12abf54ee786807a196f3a5b45
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-18 09:15:51 +0800

    Basic Routing & Controllers

---
+++ resources/views/pages/products.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>This is the id = {{$id}}</h2>
+    <h3>This is the name = <?php echo $name;?></h3>
+
+
+</body>
+
+</html>
